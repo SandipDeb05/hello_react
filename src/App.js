579d610cb718d68e085273a6757b4dc6afb278c0
SandipDeb <sandip.deb@xopuntech.com>
2022-04-02 13:41:39 +0530

    add list items: Expense Item file added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import logo from "./logo.svg";
+import ExpenseItem from "./components/ExpenseItem.jsx";
 import "./App.css";
 
 function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Toilet Paper 🧻",
+      amount: 94.12,
+      date: new Date(2020, 1, 7),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance 💰",
+      amount: 294.67,
+      date: new Date(2020, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "New TV 📺",
+      amount: 799.49,
+      date: new Date(2021, 4, 23),
+    },
+    {
+      id: "e4",
+      title: "Study Material 📚",
+      amount: 300.23,
+      date: new Date(2022, 5, 14),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Let's get started</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Hey Sandip, Happy Learning 👋</h2>
+      <p>This is React JS</p>
+      {expense.map((el) => {
+        return (
+          <ExpenseItem
+            key={el.id}
+            title={el.title}
+            amount={el.amount}
+            date={el.date}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,25 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const { date, title, amount } = props;
+
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
