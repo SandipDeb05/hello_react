efaae63d7dfe497653e52034f2b6c42096b781bf
SandipDeb <sandip.deb@xopuntech.com>
2022-04-04 09:37:14 +0530

    Add new expense to the UI is added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 import "./App.css";
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper 🧻",
+    amount: 94.12,
+    date: new Date(2020, 1, 7),
+  },
+  {
+    id: "e2",
+    title: "Car Insurance 💰",
+    amount: 294.67,
+    date: new Date(2020, 2, 28),
+  },
+  {
+    id: "e3",
+    title: "New TV 📺",
+    amount: 799.49,
+    date: new Date(2021, 4, 23),
+  },
+  {
+    id: "e4",
+    title: "Study Material 📚",
+    amount: 300.23,
+    date: new Date(2022, 5, 14),
+  },
+];
 
 const App = () => {
-  const expense = [
-    {
-      id: "e1",
-      title: "Toilet Paper 🧻",
-      amount: 94.12,
-      date: new Date(2020, 1, 7),
-    },
-    {
-      id: "e2",
-      title: "Car Insurance 💰",
-      amount: 294.67,
-      date: new Date(2020, 2, 28),
-    },
-    {
-      id: "e3",
-      title: "New TV 📺",
-      amount: 799.49,
-      date: new Date(2021, 4, 23),
-    },
-    {
-      id: "e4",
-      title: "Study Material 📚",
-      amount: 300.23,
-      date: new Date(2022, 5, 14),
-    },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense, "app.js");
+    // TODO Good practice of updating data
+    setExpenses((prev) => {
+      return [expense, ...prev];
+    });
   };
 
   // TODO React JSX under the hood
@@ -50,7 +54,7 @@ const App = () => {
         Hey Sandip, Happy Learning 👋
       </h2>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expense} />
+      <Expenses expenses={expenses} />
     </div>
   );
 };
--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -74,7 +74,7 @@ const ExpenseForm = (props) => {
           <label htmlFor="">Date</label>
           <input
             type="date"
-            min="2022-01-01"
+            min="2019-01-01"
             max="2023-12-31"
             value={enteredDate}
             onChange={dateChangeHandler}
